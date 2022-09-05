8055b73629bcaa4e91d5285bfda89c260c26bbcf
Thomasvidela <thomasvidela16@gmail.com>
2022-09-05 16:11:03 -0300

    mi sitio web

---
+++ includes/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Copatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reclamos</title>
+    <link rel="shortcute icon" href="mega.png">
+   
+    <!--- Boostrap 4 --->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+
+    <!-- FONT AWESOMW 5 -->
+    <script src="https://kit.fontawesome.com/b513e33fce.js" crossorigin="anonymous"></script> 
+   
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+<body>
+
+<nav class="navbar navbar-dark bg-dark" id="barra">
+    <div class="container">
+        <a href="index.php" class="navbar-brand">
+            <img src="mega.png" alt="" width="36" height="36">
+            Reclamos
+        </a>
+    </div>
+</nav>
